78226ff5b0bed1421005200d4726f15aff181c6f
Анатолий Пушкарев <anatoliypuss5@gmail.com>
2020-09-24 22:36:10 +0300

    Basic structures done

---
+++ index.js
@@ -0,0 +1,38 @@
+let slide1 = {
+  width: '800px',
+  height: '600px',
+  position: 1,
+  objects: {
+    text: [
+      {
+        id: 1, 
+        data: 'Hello, this is slide number 1!', 
+        font: 'Open Sans', 
+        size: '15px', 
+        color: '#000', 
+        style: null, 
+        weight: 400, 
+        decoration: null
+      }
+    ],
+    img: [
+      {
+        id: 2,
+        path: './img/imageFromSlide1.jpg',
+        width: '100px',
+        height: '100px'
+      }
+    ]
+  },
+  backgroundImage: './bg.jpg',
+  backgroundColor: null
+};
+
+let app = {
+  currSlide: 1,
+  slides: [ slide1 ],
+  settings: null,
+  history: null,
+  choosedObjectId: 1
+};
+
