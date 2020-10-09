28002d89e6d5dc4ef1415fcaf50ab2558e88c6e9
Анатолий Пушкарев <anatoliypuss5@gmail.com>
2020-10-09 22:37:49 +0300

    Basic structure made!

---
+++ src/view/slideCarousel/SlideCarousel.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './slideCarousel.css';
+
+export default function SlideCarousel() {
+  return (
+    <div id="slide-carousel"></div>
+  )
+}
---
+++ src/view/slideViewport/SlideViewport.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './slideViewport.css';
+
+export default function SlideViewport() {
+  return (
+    <div id="slide-viewport">
+      
+    </div>
+  );
+}
