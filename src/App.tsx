9e6406d17a1b2904674d4e7c6933ca46078eeada
Анатолий Пушкарев <anatoliypuss5@gmail.com>
2020-10-09 23:04:06 +0300

    Little structure corrections

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Topbar from './view/topbar/Topbar';
 import SlideCarousel from './view/slideCarousel/SlideCarousel';
 import SlideViewport from './view/slideViewport/SlideViewport';
+import Instruments from './view/instruments/Instruments';
 
 function App() {
   return (
     <div id="app">
       <Topbar />
+      <Instruments />
       <div className="working-area">
         <SlideCarousel />
         <SlideViewport />
---
+++ src/view/instruments/Instruments.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './instruments.css';
+
+export default function Instruments() {
+  return (
+    <div id="instruments"></div>
+  )
+}
