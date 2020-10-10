66b1cccf1af31152f1ab9f25adb9d0f85ece81b6
Анатолий Пушкарев <anatoliypuss5@gmail.com>
2020-10-10 07:47:02 +0300

    New features added

--- src/view/topbar/Topbar.tsx
+++ src/view/topbar/Topbar.tsx
@@ -9,6 +9,7 @@ export default function Topbar(props: TopbarProps) {
   return (
     <div id='topbar'>
       <h1>{props.presentationName}</h1>
+      <button id='export-btn'>Экспорт</button>
     </div>
   );
 }
