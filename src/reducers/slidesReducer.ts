1e3c6ef1480ed7c8e64b9f5e93569a83287b2a2d
Anatoliy <anatoliypuss5@gmail.com>
2021-01-04 08:45:35 +0300

    test commit

--- src/reducers/slidesReducer.ts
+++ src/reducers/slidesReducer.ts
@@ -1,5 +1,5 @@
 import { choosedObjectType, SlideType } from '../model/model';
 
 export default function slidesReducer(state: Array<SlideType> = [], action: any, currSlideId: string | null, choosedObject: choosedObjectType): Array<SlideType> {
-
+    return state
 }
